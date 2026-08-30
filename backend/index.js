6756c3cdbe44060181b1b5698935c52e383d0898
janvi-chhirang <153886981+janvi-chhirang@users.noreply.github.com>
2026-08-30 22:45:51 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dashbite-isyw.onrender.com",
     credentials: true,
   })
 );
